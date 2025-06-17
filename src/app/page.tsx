3bd23fcda063e72dcbc551eff1cae135deda46ed
MsPritee <badgujarpriti5@gmail.com>
2025-06-17 22:04:19 +0530

    updated page file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,28 @@ import Image from "next/image";
 import Hero from "@/components/Hero";
 import Stats from "@/components/Stats";
 import About from "@/components/About";
+import Services from "@/components/Services";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
+      {/* Hero Section */}
       <Hero />
+
+      {/* Services Section */}
+      <Services />
 
       {/* About Section */}
       <About />
 
+      {/* Stats Section */}
       <Stats />
+
+      {/* Contact Section */}
+      <Contact />
+
+    
     </main>
   );
 }
